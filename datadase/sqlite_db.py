f09ccb20a4ddb58b6b63fe20323fd6212205b9c0
Justzlobin <earthshaker.lev.25@gmail.com>
2022-12-23 23:28:00 +0200

    changes in handlers, 146

--- datadase/sqlite_db.py
+++ datadase/sqlite_db.py
@@ -140,7 +140,8 @@ def select_address_from_users(user_id):
 def create_new_custom(user_id):
     new_custom = check_list_order_id() + 1
     cur.execute("""INSERT INTO list (list_id, user_id) VALUES (%s, %s)""",
-                (str(new_custom), str(user_id)))
+                (new_custom, user_id))
+    conn.commit()
     return new_custom
 
 
