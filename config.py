5f10bb95bd93d0001d0bb8fadb3c0b0fa99f28a3
Justzlobin <earthshaker.lev.25@gmail.com>
2022-12-23 22:05:36 +0200

    changes in handlers, 146

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-#name bot = Pepsi_Order_bot
+# name bot = Pepsi_Order_bot
 TOKEN = os.getenv('TOKEN')
-ADMIN = str(os.getenv('ADMIN'))
+ADMIN = os.getenv('ADMIN')
 DB_TOKEN = os.getenv('DB_TOKEN')
