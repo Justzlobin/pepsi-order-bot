e47dc3bc382831244199f46ce107bcf34e85032e
Justzlobin <earthshaker.lev.25@gmail.com>
2022-12-27 22:55:04 +0200

    changes in handlers

--- handlers/handler.py
+++ handlers/handler.py
@@ -337,9 +337,9 @@ async def order_status_blocked_limit(query: types.CallbackQuery, callback_data: 
     await query.message.delete()
 
 
-async def back_to_menu_from_order(message: types.Message, query: types.CallbackQuery):
+async def back_to_menu_from_order(message: types.Message):
     user_data[f'{message.from_user.id}'] = None
-    await query.message.delete()
+    await message.delete()
     await message.answer(reply_markup=kb_menu, text='Ви повернулись в меню!')
 
 
