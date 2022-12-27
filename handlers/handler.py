7e3a2e52cb8ea8ae9c6d81c621da492b5f1131e8
Justzlobin <earthshaker.lev.25@gmail.com>
2022-12-28 00:03:01 +0200

    changes in handlers

--- handlers/handler.py
+++ handlers/handler.py
@@ -344,11 +344,9 @@ async def back_to_menu_from_order(message: types.Message):
 
 
 async def order_delete(query: types.CallbackQuery, callback_data: dict):
-    if sqlite_db.delete_order(callback_data['id']):
-        await dp.bot.send_message(text='Заявка видалена',
-                                  chat_id=query.message.chat.id)
-    else:
-        pass
+    sqlite_db.delete_order(callback_data['id'])
+    await dp.bot.send_message(text='Заявка видалена',
+                              chat_id=query.message.chat.id)
 
 
 def register_handlers_handler(dp: Dispatcher):
