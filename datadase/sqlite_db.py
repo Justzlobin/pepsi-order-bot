9575ce75eea1e71f014a23db765b7bdb42b882e0
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-01 22:25:40 +0200

    changes in handler

--- datadase/sqlite_db.py
+++ datadase/sqlite_db.py
@@ -283,7 +283,7 @@ def order_verification(order_id):
 
 
 def delete_not_verification():
-    cur.execute("""DELETE FROM list WHERE check_order NOT IN (%s)""", (True,))
+    cur.execute("""DELETE FROM list WHERE check_order NOT IN (%s)""", ('true',))
     return conn.commit()
 
 
