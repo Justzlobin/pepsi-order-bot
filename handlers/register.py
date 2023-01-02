5e50f77c49ed2d761364ff5a9c87ba8fc3aa5267
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-02 20:20:10 +0200

    changes in register.py

--- handlers/register.py
+++ handlers/register.py
@@ -13,36 +13,49 @@ class UserRegister(StatesGroup):
 
 async def cm_start(message: types.Message):
     await UserRegister.full_name.set()
-    await message.reply("Введіть прізвище, ім'я, по-батькові ФОП", reply_markup=back_from_register_markup())
+    await message.reply("Введіть прізвище, ім'я, по-батькові ФОП", reply_markup=kb_back_to_menu_markup)
 
 
 async def write_full_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['full_name'] = message.text
-        print(data)
+    if check_message(message.text):
+        await state.finish()
+    else:
+        async with state.proxy() as data:
+            data['full_name'] = message.text
+            print(data)
     await UserRegister.next()
     await message.reply('І ще адресу доставки:\n Приклад: "м.Вінниця, Пирогова, 119"',
-                        reply_markup=back_from_register_markup())
+                        reply_markup=kb_back_to_menu_markup)
 
 
 async def write_address(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['address'] = message.text
-        print(data)
+    if check_message(message.text):
+        await state.finish()
+    else:
+        async with state.proxy() as data:
+            data['address'] = message.text
+            print(data)
     await sqlite_db.add_user(state, message.from_user.id)
     await state.finish()
-    await message.answer(text='Тепер можна робити замовлення', reply_markup=kb_menu)
+    await message.answer(text='Тепер можна робити замовлення', reply_markup=kb_back_to_menu_markup)
 
 
-async def fsc_close(query: types.CallbackQuery, state: FSMContext):
+# async def fsc_close(query: types.CallbackQuery, state: FSMContext):
+#     await state.reset_state()
+#     await query.message.answer(reply_markup=kb_menu, text='Ви повернулись в меню')
 
-    await state.reset_state()
-    await query.message.answer(reply_markup=kb_menu, text='Ви повернулись в меню')
+
+def check_message(message):
+    if message == '🔙 Назад до меню':
+
+        return True
+    else:
+        pass
 
 
 def register_admin_handlers(dp: Dispatcher):
     dp.register_message_handler(cm_start, text='📋 Реєстрація', state=None)
     dp.register_message_handler(write_full_name, state=UserRegister.full_name)
     dp.register_message_handler(write_address, state=UserRegister.address)
-    dp.register_callback_query_handler(fsc_close,
-                                       cat_cb.filter(action='back_to_menu_from_register'))
+# dp.register_callback_query_handler(fsc_close,
+#    cat_cb.filter(action='back_to_menu_from_register'))
