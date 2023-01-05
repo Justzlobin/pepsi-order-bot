3644be9ccec3e4ab0876392b156e684366f7e3e8
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-05 22:34:24 +0200

    add new register for user

--- handlers/register_handler.py
+++ handlers/register_handler.py
@@ -1,4 +1,3 @@
-import types
 
 from aiogram.dispatcher import FSMContext
 from states import UserRegisterName
@@ -13,10 +12,21 @@ async def user_register(message: types.Message):
     await message.answer(text='register: ', reply_markup=user_register_kb())
 
 
-# async def user_register_name(query: types.CallbackQuery, state: FSMContext):
-#     await query.message.answer(text='Введіть імя')
-#     await state.set_state(UserRegisterName.user_choosing_name)
+async def user_register_name(query: types.CallbackQuery, state: FSMContext):
+    await query.message.answer(text='Введіть імя')
+    await state.set_state(UserRegisterName.user_choosing_name)
+    print(state.get_state())
+
+
+async def name_chosen(message: types.Message, state: FSMContext):
+    await state.update_data(user_name=message.text.lower())
+    await message.answer(
+        text="дякую",
+    )
+    await state.finish()
 
 
 def register_register_handlers(dp: Dispatcher):
     dp.register_message_handler(user_register, text='📋 Реєстрація')
+    dp.register_callback_query_handler(user_register_name, callback=cat_cb.filter(action='register_user_name'))
+    dp.register_message_handler(name_chosen, UserRegisterName.user_choosing_name)
