cbdbfc00329bbc6f0f45abed156ed8d666fdbd8f
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-12 19:23:08 +0200

    delete all ReplyKeyboard add new Inline

--- keyboards/menu_kb.py
+++ keyboards/menu_kb.py
@@ -28,8 +28,7 @@ def back_to_menu(designer=False):
 
 
 def combo_menu_kb():
-    combo_buttons = [
-        [i for i in menu_kb(designer=True)],
-        [back_to_menu(designer=True)]
-    ]
+    combo_buttons = []
+    [combo_buttons.append(i) for i in menu_kb(designer=True)]
+    [combo_buttons.append(i) for i in back_to_menu(designer=True)]
     return InlineKeyboardMarkup(inline_keyboard=combo_buttons)
