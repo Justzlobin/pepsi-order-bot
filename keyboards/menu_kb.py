ae702f44bd0cde95b1e6ba2e949ea496e3c3f4c1
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-12 18:56:34 +0200

    delete all ReplyKeyboard add new Inline

--- keyboards/menu_kb.py
+++ keyboards/menu_kb.py
@@ -7,8 +7,8 @@ Menu_KB = CallbackData('title', 'action')
 
 def menu_kb():
     buttons = [
-        [types.InlineKeyboardButton('Замовлення', action='new_order')],
-        [types.InlineKeyboardButton('Історія замовлень', action='last_orders')],
-        [types.InlineKeyboardButton('Реєстрація', action='register')]
+        [types.InlineKeyboardButton('Замовлення', callback_data=Menu_KB.new(action='new_order'))],
+        [types.InlineKeyboardButton('Історія замовлень', callback_data=Menu_KB.new(action='last_orders'))],
+        [types.InlineKeyboardButton('Реєстрація', callback_data=Menu_KB.new(action='register'))]
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
