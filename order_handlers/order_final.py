68064263f091999e7a14afeacdfd89e8d9af925b
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-12 23:44:05 +0200

    delete all ReplyKeyboard add new Inline

--- order_handlers/order_final.py
+++ order_handlers/order_final.py
@@ -23,6 +23,6 @@ async def order_continue(query: types.CallbackQuery):
 
 
 def register_order_final(dp: Dispatcher):
-    dp.register_callback_query_handler(add_in_list_orders, cat_cb.filter(action='add_full_order'))
-    dp.register_callback_query_handler(delete_from_order, cat_cb.filter(action='delete_from_order'))
-    dp.register_callback_query_handler(order_continue, cat_cb.filter(action='continue_from_order'))
+    dp.register_callback_query_handler(add_in_list_orders, Cat_KB.filter(action='add_full_order'))
+    dp.register_callback_query_handler(delete_from_order, Cat_KB.filter(action='delete_from_order'))
+    dp.register_callback_query_handler(order_continue, Cat_KB.filter(action='continue_from_order'))
