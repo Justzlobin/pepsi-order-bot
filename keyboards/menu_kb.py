a4fcac715d3071d69308877b0b14c55956bf7ff5
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-12 19:27:04 +0200

    delete all ReplyKeyboard add new Inline

--- keyboards/menu_kb.py
+++ keyboards/menu_kb.py
@@ -30,4 +30,4 @@ def back_to_menu(designer=False):
 def combo_menu_kb():
     combo_buttons = []
     [combo_buttons.append(i) for i in menu_kb(designer=True) and back_to_menu(designer=True)]
-    return combo_buttons
+    return InlineKeyboardMarkup(inline_keyboard=combo_buttons)
