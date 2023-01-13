2867d22f5f70c795eb7d84180b9942642b09cc90
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-13 22:57:00 +0200

    delete all ReplyKeyboard add new Inline

--- user_handlers/assortment_handler.py
+++ user_handlers/assortment_handler.py
@@ -3,10 +3,10 @@ from create_bot import dp
 from keyboards import *
 
 
-async def show_assortment(query: types.CallbackQuery, callback_data: dict):
+async def show_assortment(query: types.CallbackQuery):
     await query.bot.send_message(text='Доступні категорії:',
                                  chat_id=query.message.chat.id,
-                                 reply_markup=brand_markup(callback_data['id']))
+                                 reply_markup=cat_markup(assortment=True))
 
 
 def register_assortment_handlers(dp: Dispatcher):
