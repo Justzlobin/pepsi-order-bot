4f52477b1bfdaf1a02d6d5f8f0383e51e4465f1c
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-13 21:07:17 +0200

    delete all ReplyKeyboard add new Inline

--- user_handlers/register_handler.py
+++ user_handlers/register_handler.py
@@ -16,15 +16,14 @@ async def stop_register(query: types.CallbackQuery, state: FSMContext):
 async def user_register(query: types.CallbackQuery):
     await query.bot.send_message(text='Ваші данні: ', reply_markup=user_register_kb(query.from_user.id),
                                  chat_id=query.message.chat.id)
-    await query.message.delete()
+    await query.message.delete_reply_markup()
 
 
 async def user_register_name(query: types.CallbackQuery):
     await query.message.answer(text='Введіть ПІБ ФОП',
                                reply_markup=cancel_state(register=True))
     await UserRegisterName.user_enter_name.set()
-    await query.bot.delete_message(chat_id=query.message.chat.id,
-                                   message_id=query.message.message_id)
+    await query.message.delete_reply_markup()
 
 
 async def user_register_address(query: types.CallbackQuery):
