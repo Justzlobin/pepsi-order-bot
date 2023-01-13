73618cd13f8adac51965ca8bd21cb3e6025754e0
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-13 21:39:03 +0200

    delete all ReplyKeyboard add new Inline

--- user_handlers/register_handler.py
+++ user_handlers/register_handler.py
@@ -18,6 +18,8 @@ async def stop_register(query: types.CallbackQuery, state: FSMContext):
 async def user_register(query: types.CallbackQuery):
     await query.bot.send_message(text='Ваші данні: ', reply_markup=user_register_kb(query.from_user.id),
                                  chat_id=query.message.chat.id)
+    message_on_delete['message_id'] = query.message.message_id
+    print(message_on_delete)
     await query.message.delete()
 
 
@@ -25,8 +27,6 @@ async def user_register_name(query: types.CallbackQuery):
     await query.message.answer(text='Введіть ПІБ ФОП',
                                reply_markup=cancel_state(register=True))
     await UserRegisterName.user_enter_name.set()
-    message_on_delete['message_id'] = query.message.message_id
-    print(message_on_delete)
     await query.message.delete()
 
 
@@ -35,8 +35,6 @@ async def user_register_address(query: types.CallbackQuery):
                                     'Приклад: м.Вінниця, Пирогова, 100',
                                reply_markup=cancel_state(register=True))
     await UserRegisterName.user_enter_address.set()
-    message_on_delete['message_id'] = query.message.message_id
-    print(message_on_delete)
     await query.message.delete()
 
 
