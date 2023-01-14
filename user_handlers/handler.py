cda390c38b207c8b5f05d0750bb69b6712161ccb
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-14 22:29:14 +0200

    add photo

--- user_handlers/handler.py
+++ user_handlers/handler.py
@@ -93,8 +93,8 @@ async def cmd_numbers(query: types.CallbackQuery, callback_data: dict):
 
     text = sqlite_db.select_one_position(callback_data['id'])
     full_text = f'{text[0]} {text[1]} {text[2]} {text[3]} {text[4]}'
-    await query.bot.send_photo(chat_id=query.message.chat.id,
-                               photo='image/77.png')
+    # await query.bot.send_photo(chat_id=query.message.chat.id,
+    #                            photo='image/77.png')
     await query.message.answer(text=f'{full_text}\n'
                                     f'Кількість: 0, Ціна: {text[5]}'
                                , reply_markup=keyboard(callback_data['id']))
