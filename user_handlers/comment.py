87a540b7b1e0e4ea76c65c9dcd0ef3180ceb2608
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-14 14:54:25 +0200

    delete all ReplyKeyboard add new Inline

--- user_handlers/comment.py
+++ user_handlers/comment.py
@@ -12,23 +12,25 @@ message_delete = {}
 async def comment(query: types.CallbackQuery):
     await CommentToOrder.write_comment.set()
     message_delete['message'] = await dp.bot.send_message(chat_id=query.message.chat.id,
-                                     text='Введіть примітку.\n'
-                                          'Приклад:\n'
-                                          '"Штрих" - штрихкоди\n'
-                                          '"Серт" - сертифікат\n'
-                                          '"ттн" - товаро-транспортна накладна\n'
-                                          'Щоб скасувати дію натисність /cancel')
+                                                          text='Введіть примітку.\n'
+                                                               'Приклад:\n'
+                                                               '"Штрих" - штрихкоди\n'
+                                                               '"Серт" - сертифікат\n'
+                                                               '"ттн" - товаро-транспортна накладна\n',
+                                                          reply_markup=cancel_state())
+    # 'Щоб скасувати дію натисність /cancel')
     await query.message.delete()
 
 
-async def stop_comment(message: types.Message, state: FSMContext):
-    # async def stop_comment(query: types.CallbackQuery, state: FSMContext):
+# async def stop_comment(message: types.Message, state: FSMContext):
+async def stop_comment(query: types.CallbackQuery, state: FSMContext):
     current_state = state.get_state()
     if current_state is None:
         return
     await state.finish()
     await message_delete['message'].delete()
-    await message.answer(text='Дію скасовано!', reply_markup=order_menu_kb())
+    await query.bot.send_message(text='Дію скасовано!', reply_markup=order_menu_kb(),
+                                 chat_id=query.message.chat.id)
 
 
 async def write_comment(message: types.Message, state: FSMContext):
@@ -43,6 +45,7 @@ async def write_comment(message: types.Message, state: FSMContext):
 
 def comment_order_handlers(dp: Dispatcher):
     dp.register_callback_query_handler(comment, Cat_KB.filter(action='comment'), state=None)
-    dp.register_message_handler(stop_comment, commands='cancel', state='*')
+    dp.register_callback_query_handler(stop_comment, Cat_KB.filter(action='stop_comment'), state='*')
+    # dp.register_message_handler(stop_comment, commands='cancel', state='*')
     dp.register_message_handler(write_comment, state=CommentToOrder.write_comment)
-    # dp.register_callback_query_handler(stop_comment, Cat_KB.filter(action='stop_comment'), state='*')
+    dp.register_callback_query_handler(stop_comment, Cat_KB.filter(action='stop_comment'), state='*')
