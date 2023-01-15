6c944b9881d3cf7fd84e60e28a453fefdc0a4352
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-15 17:24:56 +0200

    add photo

--- delete/delete_message.py
+++ delete/delete_message.py
@@ -1,16 +1,27 @@
+
+
 class UnMessage:
     def __init__(self):
         self.dict_mess = {}
         self.dict_photo = {}
+        self.dict_chat = {}
 
-    def add(self, message_id):
+    def add(self, message_id, chat_id):
         self.dict_mess['message_id'] = message_id
+        self.dict_chat['chat_id'] = chat_id
 
-    def destr(self):
-        return self.dict_mess['message_id']
+    def destr(self, chat_id):
+        if chat_id == self.dict_chat['chat_id']:
+            return self.dict_mess['message_id']
+        else:
+            pass
 
-    def add_photo(self, message_id):
+    def add_photo(self, message_id, chat_id):
         self.dict_photo['message_photo'] = message_id
+        self.dict_chat['chat_id'] = chat_id
 
-    def destr_photo(self):
-        return self.dict_photo['message_photo']
+    def destr_photo(self, chat_id):
+        if chat_id == self.dict_chat['chat_id']:
+            return self.dict_mess['message_photo']
+        else:
+            pass
