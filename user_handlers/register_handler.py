df57a2caa1fd741f7c150a3c8c0d03c0845625ae
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-19 17:26:45 +0200

    try released new class 'delete last messages and set_of_messages'

--- user_handlers/register_handler.py
+++ user_handlers/register_handler.py
@@ -18,6 +18,7 @@ async def stop_register(query: types.CallbackQuery, state: FSMContext):
 
 
 async def user_register(query: types.CallbackQuery):
+    print(del_mes.chat_dict)
     chat = query.message.chat.id
     del_mes.add_message(chat_id=chat, message_id=query.message.message_id)
     for message in del_mes.chat_dict[chat][1:]:
@@ -27,7 +28,7 @@ async def user_register(query: types.CallbackQuery):
             pass
     await query.bot.send_message(text='Ваші данні: ', reply_markup=user_register_kb(query.from_user.id),
                                  chat_id=query.message.chat.id)
-
+    print(del_mes.chat_dict)
 
 async def user_register_name(query: types.CallbackQuery):
     delete_message.add(message_id=await query.message.answer(text='Введіть ПІБ ФОП',
