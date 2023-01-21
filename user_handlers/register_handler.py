ea64a0c8fe6577608a28abf9cf9d2c5b89875eb5
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-21 13:51:51 +0200

    released new class delete_message

--- user_handlers/register_handler.py
+++ user_handlers/register_handler.py
@@ -26,12 +26,7 @@ async def user_register(query: types.CallbackQuery):
                                                query.from_user.id),
                                            chat_id=query.message.chat.id)
     del_mes.add_message(chat_id=chat, message_id=message)
-
-    for message_in_dict in del_mes.chat_dict[chat][:-1]:
-        try:
-            await message_in_dict.delete()
-        except exceptions.MessageToDeleteNotFound:
-            pass
+    delete_message_from_dict(chat=chat)
 
     print(del_mes.chat_dict)
     print(del_mes.chat_dict[chat][1:])
@@ -81,6 +76,15 @@ async def address_enter(message: types.Message, state: FSMContext):
     await message.answer(text='Ваші данні оновлені', reply_markup=menu_kb())
     await delete_message.destr(message.chat.id).delete()
     await message.delete()
+
+
+def delete_message_from_dict(chat):
+    for message_in_dict in del_mes.chat_dict[chat][:-1]:
+        try:
+            await message_in_dict.delete()
+            del_mes.chat_dict[chat].remove(message_in_dict)
+        except exceptions.MessageToDeleteNotFound:
+            pass
 
 
 def register_register_handlers(dp: Dispatcher):
