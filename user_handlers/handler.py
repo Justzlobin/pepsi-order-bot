e8b079129cea3577bbf347711a37d6b52749a3dc
Justzlobin <earthshaker.lev.25@gmail.com>
2023-01-23 21:42:10 +0200

    Markdown

--- user_handlers/handler.py
+++ user_handlers/handler.py
@@ -14,7 +14,9 @@ del_mes = Count()
 
 async def command_start(message: types.Message):
     await message.delete()
-    message = await message.bot.send_message(message.from_user.id, '__PepsiBot__',
+    message = await message.bot.send_message(message.from_user.id, '#PepsiBot\n'
+                                                                   '___\n'
+                                                                   '___Опис___',
 
                                              reply_markup=menu_kb(), parse_mode='MarkdownV2')
     del_mes.add_message(chat_id=message.chat.id, message_id=message)
