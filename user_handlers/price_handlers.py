e0564b83adf085fae39bb7018465b2480d10f9e7
Justzlobin <earthshaker.lev.25@gmail.com>
2023-02-10 18:07:12 +0200

    start_to_full_refactoring

--- user_handlers/price_handlers.py
+++ user_handlers/price_handlers.py
@@ -22,7 +22,7 @@ async def price_position(query: types.CallbackQuery, callback_data: dict):
 async def price_single_position(query: types.CallbackQuery, callback_data: dict):
     text = sqlite_db.select_one_position(callback_data['id'])
     full_text = f'{text[0]} {text[1]} {text[2]} {text[3]} {text[4]}'
-    await edit_text(message=query.message, message_text=full_text, reply_markup=back_to_menu())
+    await edit_text(message=query.message, message_text=full_text, reply_markup=back_to.back_to_menu())
 
 
 def register_price_handlers(dp: Dispatcher):
