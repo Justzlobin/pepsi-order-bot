3632ec808e729f6897f50235e4bdbf8f56ad920a
Justzlobin <earthshaker.lev.25@gmail.com>
2023-02-13 22:05:55 +0200

    start_to_full_refactoring

--- datadase/sqlite_db.py
+++ datadase/sqlite_db.py
@@ -55,9 +55,9 @@ def select_cat_id(brand_id):
 
 
 def select_brand_id(pos_id):
-    print('pos' + pos_id)
-    cur.execute("""SELECT brand_id FROM position WHERE pos_id = %s""", (pos_id,))
-    print('cur' + cur.fetchone()[0])
+    print('pos ' + pos_id)
+    cur.execute("""SELECT brand_id FROM position WHERE pos_id = %s""", (int(pos_id),))
+    print('cur ' + str(cur.fetchone()[0]))
     return cur.fetchone()[0]
 
 
