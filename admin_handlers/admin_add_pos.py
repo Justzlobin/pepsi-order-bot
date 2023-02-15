fa1a9f37d7dd0bed9aa1d908ff416ac444e10e1d
Justzlobin <earthshaker.lev.25@gmail.com>
2023-02-15 20:08:32 +0200

    start_work_with_admin_panel

--- admin_handlers/admin_add_pos.py
+++ admin_handlers/admin_add_pos.py
@@ -9,11 +9,10 @@ from datadase.admin_db import select_id_title_of_category
 
 
 async def start_add_position(query: types.CallbackQuery):
-    await AdminAddPosition.category.set()
     await edit_text(message=query.message, message_text=f'Категорії:\n'
                                                         f'{select_id_title_of_category()}',
                     reply_markup=None)
-    await AdminAddPosition.states.next()
+    await AdminAddPosition.category.set()
 
 
 async def admin_add_category(message: types.Message, state: FSMContext):
@@ -21,8 +20,18 @@ async def admin_add_category(message: types.Message, state: FSMContext):
         data_position['category'] = message.text
     await edit_text(message=message, message_text=f'Бренди:\n'
                                                   f'{select_id_title_of_brand()}', reply_markup=None)
-    await AdminAddPosition.states.next()
+    await AdminAddPosition.brand.set()
+
+
+async def admin_add_brand(message: types.Message, state: FSMContext):
+    async with state.proxy() as data_position:
+        data_position['brand'] = message.text
+    await edit_text(message=message, message_text='finish', reply_markup=None)
+    print(data_position)
+    await state.finish()
 
 
 def register_admin_add_pos_handlers(dp: Dispatcher):
     dp.register_callback_query_handler(start_add_position, Admin_KB.filter(action='add_new_position'))
+    dp.register_message_handler(admin_add_category, state=AdminAddPosition.category)
+    dp.register_message_handler(admin_add_brand, state=AdminAddPosition.brand)
