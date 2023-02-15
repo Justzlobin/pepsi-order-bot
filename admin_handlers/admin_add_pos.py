73a8ebb3cc1c1661beabc60ebc8858f256a66291
Justzlobin <earthshaker.lev.25@gmail.com>
2023-02-15 22:53:46 +0200

    start_work_with_admin_panel

--- admin_handlers/admin_add_pos.py
+++ admin_handlers/admin_add_pos.py
@@ -36,10 +36,10 @@ async def admin_add_price(message: types.Message, state: FSMContext):
     async with state.proxy() as data_position:
         data_position['price'] = message.text
 
-    admin_add_new_position(data_position['brand'], data_position['tasty'], data_position['size'],
-                                     data_position['price'])
-    await message.answer(text='Done')
-
+    if admin_add_new_position(data_position['brand'], data_position['tasty'], data_position['size'],
+                                     data_position['price']):
+        await message.answer(text='Done')
+    else: await message.answer(text='error')
     await state.finish()
 
 
