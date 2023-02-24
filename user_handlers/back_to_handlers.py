8a9e5a81d79211c1adee5283c7fd1bcdf3b74d92
Justzlobin <earthshaker.lev.25@gmail.com>
2023-02-24 11:30:24 +0200

    add image in bot

--- user_handlers/back_to_handlers.py
+++ user_handlers/back_to_handlers.py
@@ -39,6 +39,7 @@ async def back_to_tasty_from_pos(query: types.CallbackQuery, callback_data: dict
     await edit_text(message=query.message, message_text='POSITIONS',
                     reply_markup=position_markup(brand_id, status.dialog_status[query.from_user.id]).add(
                         back_to_brand_from_tasty_kb(select_cat_id(brand_id))))
+    await delete_photo(query.message.chat.id)
     try:
         del order.order_dict[query.from_user.id][callback_data['id']]
     except KeyError:
