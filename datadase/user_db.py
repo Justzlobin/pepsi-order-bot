33ab9a69f21ba8672339f4b947423e289ebe1e34
Justzlobin <earthshaker.lev.25@gmail.com>
2023-02-26 19:27:12 +0200

    add registration

--- datadase/user_db.py
+++ datadase/user_db.py
@@ -6,8 +6,8 @@ conn = psycopg2.connect(DB_TOKEN, sslmode='require')
 cur = conn.cursor()
 
 
-def select_name_and_address_from_users(user_id):
-    cur.execute("""SELECT user_full_name, user_address FROM users WHERE user_id = %s""", (user_id,))
+def select_name_address_title_from_users(user_id):
+    cur.execute("""SELECT user_full_name, user_address, user_title FROM users WHERE user_id = %s""", (user_id,))
     return cur.fetchall()
 
 
