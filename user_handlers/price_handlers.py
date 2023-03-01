030a9973c49f19b4184635b58461b0be738b0861
Justzlobin <earthshaker.lev.25@gmail.com>
2023-03-01 19:41:46 +0200

    add message for user

--- user_handlers/price_handlers.py
+++ user_handlers/price_handlers.py
@@ -19,22 +19,22 @@ async def price_brand(query: types.CallbackQuery, callback_data: dict):
     if status.dialog_status[query.from_user.id] == 'price':
         await edit_text(query.message, message_text='Бренди:',
                         reply_markup=brand_markup(callback_data['id']).add(
-                            [back_to_cat_from_brand_kb(), back_to_menu_kb()]))
+                            back_to_cat_from_brand_kb(), back_to_menu_kb()))
     if status.dialog_status[query.from_user.id] == 'order':
         await edit_text(query.message, message_text='Бренди:',
                         reply_markup=brand_markup(callback_data['id']).add(
-                            [back_to_cat_from_brand_kb(), back_to_order_kb()]))
+                            back_to_cat_from_brand_kb(), back_to_order_kb()))
 
 
 async def price_tasty(query: types.CallbackQuery, callback_data: dict):
     brand_id = callback_data['id']
     if status.dialog_status[query.from_user.id] == 'price':
         await edit_text(query.message, message_text='Смаки:',
-                        reply_markup=position_markup(brand_id, status.dialog_status[query.from_user.id]).add(
+                        reply_markup=position_markup(brand_id, status.dialog_status[query.from_user.id]).row(
                             back_to_brand_from_tasty_kb(sqlite_db.select_cat_id(brand_id)), back_to_menu_kb()))
     if status.dialog_status[query.from_user.id] == 'order':
         await edit_text(query.message, message_text='Смаки:',
-                        reply_markup=position_markup(brand_id, status.dialog_status[query.from_user.id]).add(
+                        reply_markup=position_markup(brand_id, status.dialog_status[query.from_user.id]).row(
                             back_to_brand_from_tasty_kb(sqlite_db.select_cat_id(brand_id)), back_to_order_kb()))
 
 
