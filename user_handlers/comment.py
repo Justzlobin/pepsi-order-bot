d90d99f6a899c8005c0106a8234ccf71d5457cb2
Justzlobin <earthshaker.lev.25@gmail.com>
2023-03-09 23:15:54 +0200

    add calendar

--- user_handlers/comment.py
+++ user_handlers/comment.py
@@ -3,21 +3,24 @@ from aiogram import Dispatcher
 from keyboards import *
 from states.comment_states import CommentToOrder
 from user_handlers.handler import edit_text, order, status
-from text.text_in_message import menu_order
+from text.text_in_message import menu_order, main_menu
 
 comment_message = {}
 
 
 async def comment(query: types.CallbackQuery):
     await CommentToOrder.write_comment.set()
-    await edit_text(query.message,
-                    message_text='Введіть примітку.\n'
-                                 'Приклад:\n'
-                                 '<b>Дата доставки</b>\n'
-                                 '<b>"Штрих"</b> - штрихкоди\n'
-                                 '<b>"Серт"</b> - сертифікат\n'
-                                 '<b>"ТТН"</b> - товаро-транспортна накладна\n',
-                    reply_markup=cancel_state(status=status.dialog_status[query.from_user.id]))
+    try:
+        await edit_text(query.message,
+                        message_text='Введіть примітку.\n'
+                                     'Приклад:\n'
+                                     '<b>Дата доставки</b>\n'
+                                     '<b>"Штрих"</b> - штрихкоди\n'
+                                     '<b>"Серт"</b> - сертифікат\n'
+                                     '<b>"ТТН"</b> - товаро-транспортна накладна\n',
+                        reply_markup=cancel_state(status=status.dialog_status[query.from_user.id]))
+    except KeyError:
+        await edit_text(message=query.message, message_text=main_menu, reply_markup=menu_kb())
     comment_message['message'] = query.message
     print(comment_message)
 
