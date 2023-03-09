209baae018c81f24f21933f5b210232d63fdcb1a
Justzlobin <earthshaker.lev.25@gmail.com>
2023-03-09 20:40:32 +0200

    add calendar

--- order_handlers/order_settings.py
+++ order_handlers/order_settings.py
@@ -1,6 +1,6 @@
 from aiogram import Dispatcher
 from keyboards import *
-from keyboards.calendar_kb import SimpleCalendar
+from keyboards.calendar_kb import SimpleCalendar, calendar_callback as simple_cal_callback
 from user_handlers.handler import edit_text, order
 from text.text_in_message import menu_order
 
@@ -29,8 +29,18 @@ async def payment_bank(query: types.CallbackQuery):
     order.order_settings_dict(user_id=query.from_user.id, payment='💳 Банк')
 
 
+async def process_simple_calendar(callback_query: CallbackQuery, callback_data: dict):
+    selected, date = await SimpleCalendar().process_selection(callback_query, callback_data)
+    if selected:
+        await callback_query.message.answer(
+            f'You selected {date.strftime("%d/%m/%Y")}',
+            reply_markup=keyboard_settings()
+        )
+
+
 def register_order_settings(dp: Dispatcher):
     dp.register_callback_query_handler(calendar, Order_KB.filter(action='date_deliver'))
     dp.register_callback_query_handler(payment, Order_KB.filter(action='payment'))
     dp.register_callback_query_handler(payment_cash, Order_KB.filter(action='cash'))
     dp.register_callback_query_handler(payment_bank, Order_KB.filter(action='bank'))
+    dp.register_callback_query_handler(process_simple_calendar, simple_cal_callback.filter())
