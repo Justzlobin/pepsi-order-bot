a3aefd78542df43b137006997ffda063b9ee05a5
Justzlobin <earthshaker.lev.25@gmail.com>
2023-07-06 18:25:46 +0300

    add try in handler

--- accounter/accounter_other.py
+++ accounter/accounter_other.py
@@ -21,4 +21,23 @@ def stat_for_user(stat):
         if i[0] == '-':
             set_minus[i[2]] -= i[1]
 
-    return set_plus, set_minus
+    return '\n'.join(
+        [f'{st[0]} {st[1]}' for st in set_plus.items()] and [f'{st[0]} {st[1]}' for st in set_minus.items()])
+
+
+def dif_stat_for_user(stat):
+    set_stat = {}
+
+    for i in stat:
+        if i[0] == '+':
+            set_stat[i[2]] = 0
+        if i[0] == '-':
+            set_stat[i[2]] = 0
+
+    for i in stat:
+        if i[0] == '+':
+            set_stat[i[2]] += i[1]
+        if i[0] == '-':
+            set_stat[i[2]] -= i[1]
+
+    return '\n'.join([f'{st[0]} {st[1]}' for st in set_stat.items()])
--- accounter/test_accountant.py
+++ accounter/test_accountant.py
@@ -1,24 +1,22 @@
 stat = [('-', 212, 'fucking work'), ('-', 212, 'fucking work'), ('-', 111, 'food'), ('-', 111, 'food'),
         ('-', 200, 'work'), ('+', 5000, 'зарплата'), ('-', 200, 'dfqwwqe'), ('-', 200, 'work'), ('-', 200, 'work'),
-        ('-', 200, 'work'), ('-', 2000, 'work'), ('-', 150, 'work'), ('+', 21000, 'зарплата')]
+        ('-', 200, 'work'), ('-', 2000, 'work'), ('-', 150, 'work'), ('+', 21000, 'зарплата'), ('+', 200, 'work')]
 
-set_plus = {}
-set_minus = {}
-
-for i in stat:
-    if i[0] == '+':
-        set_plus[i[2]] = 0
-    if i[0] == '-':
-        set_minus[i[2]] = 0
+set_stat = {}
 
 for i in stat:
     if i[0] == '+':
-        set_plus[i[2]] += i[1]
+        set_stat[i[2]] = 0
     if i[0] == '-':
-        set_minus[i[2]] -= i[1]
+        set_stat[i[2]] = 0
 
-print(set_plus)
-print(set_minus)
+for i in stat:
+    if i[0] == '+':
+        set_stat[i[2]] += i[1]
+    if i[0] == '-':
+        set_stat[i[2]] -= i[1]
 
 
+text = '\n'.join([f'{st[0]} {st[1]}' for st in set_stat.items()])
 
+print(text)
